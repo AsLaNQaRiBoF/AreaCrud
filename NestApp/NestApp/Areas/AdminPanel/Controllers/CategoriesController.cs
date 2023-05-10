64fd624969a1641f81fceb9b176abbcb563ad6bf
aslan <aslanqaribov7@gmail.com>
2023-05-10 10:43:07 +0400

    first

---
+++ NestApp/NestApp/Areas/AdminPanel/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NestApp.DAL;
+using NestApp.Models;
+
+namespace NestApp.Areas.AdminPanel.Controllers
+{
+	[Area("AdminPanel")]
+
+	public class CategoriesController : Controller
+	{
+		private readonly AppDbContext _context;
+		public CategoriesController(AppDbContext context)
+		{
+			_context=context;
+		}
+
+
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Categories.ToListAsync());
+		}
+		public async Task<IActionResult> Detail(int id)
+		{
+			return View(await _context.Categories.FirstOrDefaultAsync(x => x.Id == id));
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+	}
+}
+ 
---
+++ NestApp/NestApp/Areas/AdminPanel/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NestApp.DAL;
+
+namespace NestApp.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPAnel")]
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+        public ProductsController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products
+                .Include(x => x.Category)
+                .Include(x => x.ProductImages)
+                .Where(x => x.IsDeleted == false)
+                .OrderByDescending(p => p.Id)
+                .Take(10).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+            return View();
+        }
+    }
+    }
